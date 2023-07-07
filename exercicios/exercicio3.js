f457ff5f585d1713195c17b33d363e4053c22fdc
Anderson <eusouanderson@outlook.com>
2023-07-07 07:25:47 -0300

    "update"

---
+++ exercicios/exercicio3.js
@@ -0,0 +1,28 @@
+﻿/*
+Exercício 3
+
+Nome: Input de Informações
+Objetivo: Receber dados do usuário, trabalhar com os valores e exibir para o usuário.
+Dificuldade: Principiante
+
+1 - Crie um programa que receba do usuário seu nome, idade e gênero;
+2 - Exiba na tela seguinte mensagem: "Olá, {nome}, você possui {idade} anos de idade e é do gênero {genero}. Já pensou no que você fará no seu aniversário de {idade + 1} anos?".
+*/
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite seu nome: ", (nome) => {
+
+    rl.question("Digite sua idade: ", (idade) => {
+    
+        rl.question("Digite seu gênero (M/F): ", (genero) => {
+
+            console.log(`Olá, ${nome}, você possui ${idade} anos de idade e é do gênero ${genero}. Já pensou no que você fará no seu aniversário de ${idade + 1} anos?`);
+        });
+    });
+
+});
--- exercicios/exercicio3.py
+++ exercicios/exercicio3.py
@@ -8,3 +8,11 @@ Dificuldade: Principiante
 1 - Crie um programa que receba do usuário seu nome, idade e gênero;
 2 - Exiba na tela seguinte mensagem: "Olá, {nome}, você possui {idade} anos de idade e é do gênero {genero}. Já pensou no que você fará no seu aniversário de {idade + 1} anos?".
 """
+
+nome = str(input("Qual é o seu nome? "))
+
+idade = int(input("Quantos anos você tem? "))
+
+genero = str(input("Qual é o seu gênero? (Escreva por extenso, exemplo: masculino) "))
+
+print(f"Olá, {nome}, você possui {idade} anos de idade e é do gênero {genero}. Já pensou no que você fará no seu aniversário de {idade + 1} anos?")
