4822d42a65df0f8113b640c6e3e0dce61aeb6a6a
Anderson <eusouanderson@outlook.com>
2023-07-26 10:36:55 -0300

    "update"

---
+++ exercicios/exercicio1.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Ola Kotlin")
+
+}
+
+// Declaração de variável
+fun cont(){
+    val number : Int = 2
+    number++
+    println(number)
+}
+
+fun variables() {
+    val number : Int = 1
+    val string : String = "Yes! your String"
+    val double : Double = 4.25
+    val bolean : Bolean = true
+
+    println(number, $string , $double, $bolean)
+
+}
---
+++ exercicios/exercicio3c.js
@@ -0,0 +1,24 @@
+﻿function soma(valor1, valor2) {
+  return valor1 + valor2;
+}
+
+function comissao(vendas) {
+  if (vendas > 5000) {
+    return vendas * 1.2;
+  } else if (vendas > 1000) {
+    return vendas * 1.1;
+  } else {
+    return vendas;
+  }
+}
+
+function calculoComissao() {
+  vendasProdutos = 1200;
+  vendasServicos = 500;
+  vendasAssessorios = 300;
+  totalVendas = soma(vendasProdutos, vendasServicos) + vendasAssessorios;
+
+  return comissao(totalVendas);
+}
+
+console.log(calculoComissao());
